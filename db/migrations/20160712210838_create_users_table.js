c6bbca23d7a64f38112f71453ab0e56ed4d3db2c
Curtis Bateson <curtislbateson@gmail.com>
2017-10-27 23:45:52 +0000

    Building on views, added scss

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,10 +1,11 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
-  });
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
-};
+    return knex.schema.createTable('users', function (table) {
+      table.increments();
+      table.string('name');
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  
