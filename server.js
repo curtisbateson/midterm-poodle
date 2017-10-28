0cad273c24471b49ca9b3dfbeb7aea5daf0ee3bf
Ilinca Pascu <xipascu@gmail.com>
2017-10-28 23:19:29 +0000

    Timepicker working, but nothing working.

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static("public"));
 app.use("/events", eventRoutes(knex))
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
