e30d58a11c46750f4b58bdafba877d09e186d29e
Greg Sugiyama <gjsugiyama@gmail.com>
2017-10-29 02:24:47 +0000

    hooked up functions to routes

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const express       = require('express');
-const router        = express.Router();
-const generateId    = require('../public/scripts/generateID.js')
-const session       = require('cookie-session')
-const app           = express();
-const getEvent     = require('../public/scripts/getEvent.js')
+const express         = require('express');
+const router          = express.Router();
+const app             = express();
+const session         = require('cookie-session')
+const getEvent        = require('../public/scripts/getEvent.js')
+const generateId      = require('../public/scripts/generateID.js')
+const insertDatabase  = require('../public/scripts/insertDatabase.js')
+const addAttendee     = require('../public/scripts/addAttendee.js')
 
 module.exports = (knex) => {
 
@@ -15,28 +17,13 @@ module.exports = (knex) => {
 
   router.post("/", (req, res) => {
     let longId = generateId();
-
     req.session.event = {
-    description: req.body.event_description,
-    title: req.body.event_title,
-    longId: longId
+      description: req.body.description,
+      title: req.body.title,
+      longId: longId
     }
-
-    req.session.dates = {
-      dateOne : {
-          timeOne: "8:00",
-          timeTwo: "9:00",
-          TimeThree: "10:00"
-        },
-        dateTwo : {
-          timeOne: "8:00",
-          timeTwo: "9:00",
-          TimeThree: "10:00"
-        }
-      }
-      console.log(req.session)
-
-    res.redirect("/events/" + longId + "/dates")        
+  
+     res.redirect("/events/" + longId + "/dates")        
   })                    
         
   router.get("/:id/dates", (req, res) => {
@@ -50,9 +37,10 @@ module.exports = (knex) => {
   })
 
   router.get("/:id/organizer", (req, res) => {
+    let id = req.session.event.longId
+   
     
-    
-    res.render("owner")
+    res.render("organizer", {id})
   })
 
   router.post("/:id/organizer", (req, res) => {
@@ -60,11 +48,13 @@ module.exports = (knex) => {
       name: req.body.name,
       email: req.body.email
     }
+    insertDatabase(req.session, knex)
     res.redirect("/:id")
 
   })
 
   router.get("/:id", (req, res) => {
+
   })
 
   router.post("/:id", (req, res) => {
